693c2a2fd7e44bd0e4300a354ab6efd096851d3c
Boekemeyer <tboekemeyer@hotmail.de>
2017-05-23 15:17:20 +0200

    added loads of textures

---
+++ Texture Practice/Assets/_Scripts/Wiggle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggle : MonoBehaviour {
+
+    public float duration;
+    public float magnitude;
+
+    public void Update()
+    {
+        Shake();
+
+    }
+
+    IEnumerator Shake()
+    {
+
+        float elapsed = 0.0f;
+
+        Vector3 originalPos = transform.position;
+
+        while (elapsed < duration)
+        {
+
+            elapsed += Time.deltaTime;
+
+            float percentComplete = elapsed / duration;
+            float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+            // map value to [-1, 1]
+            float x = Random.value * 2.0f - 1.0f;
+            float y = Random.value * 2.0f - 1.0f;
+            x *= magnitude * damper;
+            y *= magnitude * damper;
+
+            transform.position = new Vector3(x, y, originalPos.z);
+
+            yield return null;
+        }
+
+        transform.position = originalPos;
+    }
+
+}
